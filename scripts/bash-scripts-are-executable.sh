7fcd161a6716d0da0ad2060dca8443d83199db31
Samuele Maci <maci@yelp.com>
2018-09-09 21:12:59 +0200

    Fix 'all shell script have to be executable' script

--- scripts/bash-scripts-are-executable.sh
+++ scripts/bash-scripts-are-executable.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -euo pipefail -o posix -o functrace
 
-declare -a not_executable_bash_scripts
-
-for file in "$@"; do
+not_executable_bash_scripts=$(for file in "$@"; do
     if [ ! -x "${file}" ]; then
-        not_executable_bash_scripts+=("${file}")
+	echo -n "${file} "
     fi
-done
+done)
 
-if [ ${#not_executable_bash_scripts[@]} -ne 0 ]; then
-    echo "The following scripts are not executable: ${not_executable_bash_scripts[*]}" > /dev/stderr
+if [ "${not_executable_bash_scripts}" != "" ]; then
+    echo "The following scripts are not executable: ${not_executable_bash_scripts}" > /dev/stderr
     exit 1
 else
     exit 0
